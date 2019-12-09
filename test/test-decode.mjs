6eb9e27f9788b927e42b9e967fdb18060f6964af
Myles Borins <mylesborins@google.com>
2019-12-09 02:05:59 -0500

    test: modify test-decode

--- test/test-decode.mjs
+++ test/test-decode.mjs
@@ -1,9 +1,9 @@
 import { test } from './util.mjs';
 
-// var decode = require('../cjs/decode.js');
+// import decode from '../lib/decode.mjs';
 
 test('decode: basic', (t) => {
-  // const buf = new Buffer('/test\0/route\0,i\0,123\0');
+  // const buf = Buffer.from('/test\0\0,iiis\0');
   // t.deepEquals(decode(buf), ['test', 'i', 123], `should be ${buf}`);
   t.done();
 });
